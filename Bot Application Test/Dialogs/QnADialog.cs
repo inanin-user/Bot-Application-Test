a31f2f1dd19aaf3263657fb325cf8eaf101d0fc1
inanin support <user.inanin@gmail.com>
2017-07-26 11:58:28 +0800

    Add project files.

---
+++ Bot Application Test/Dialogs/QnADialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace QnAMakerDialog.Sample.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("8f833e867b25443b95d8c23cd367f7ce", "258dbfdd-2470-46ae-ae1f-8a2354d31d80")]
+    public class QnADialog : QnAMakerDialog<object>
+    {
+        /// <summary>
+        /// Handler used when the QnAMaker finds no appropriate answer
+        /// </summary>
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
+            context.Wait(MessageReceived);
+        }
+
+        /// <summary>
+        /// This is the default handler used if no specific applicable score handlers are found
+        /// </summary>
+        public override async Task DefaultMatchHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            // ProcessResultAndCreateMessageActivity will remove any attachment markup from the results answer
+            // and add any attachments to a new message activity with the message activity text set by default
+            // to the answer property from the result
+            var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
+            messageActivity.Text = $"I found an answer that might help...{result.Answer}.";
+
+            await context.PostAsync(messageActivity);
+
+            context.Wait(MessageReceived);
+        }
+
+        /// <summary>
+        /// Handler to respond when QnAMakerResult score is a maximum of 50
+        /// </summary>
+        [QnAMakerResponseHandler(50)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            var messageActivity = ProcessResultAndCreateMessageActivity(context, ref result);
+            messageActivity.Text = $"I found an answer that might help...{result.Answer}.";
+            await context.PostAsync(messageActivity);
+
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ Bot Application Test/Dialogs/RootDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System.Web.Compilation;
+using static Microsoft.Bot.Builder.Azure.Utils;
+using System.Diagnostics;
+
+namespace Bot_Application_Test.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            switch (activity.GetActivityType())
+            {
+                case ActivityTypes.Message:
+                    try
+                    {
+                        //await Conversation.SendAsync(activity, () => new QnADialog());
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                    break;
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+
+}
